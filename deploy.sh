808314335d23e976c7ab9a2b5dcbdc196af97d4b
Abbas <abs.hallalit@gmail.com>
2021-01-04 11:46:50 +0000

    upload files

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
-docker build -t abbas/multi-client:latest -t  abbas/multi-client:$SHA -f client/Dockerfile client/
-docker build -t abbas/multi-server:latest -t abbas/multi-server:$SHA -f server/Dockerfile server/
-docker build -t abbas/multi-workeri:lastest -t abbas/multi-worker:$SHA -f worker/Dockerfile worker/
+docker build -t absh12/multi-client:latest -t  absh12/multi-client:$SHA -f client/Dockerfile client/
+docker build -t absh12/multi-server:latest -t absh12/multi-server:$SHA -f server/Dockerfile server/
+docker build -t absh12/multi-workeri:lastest -t absh12/multi-worker:$SHA -f worker/Dockerfile worker/
 
-docker push abbas/multi-client:latest
-docker push abbas/multi-server:latest
-docker push abbas/multi-workeri:lastest
+docker push absh12/multi-client:latest
+docker push absh12/multi-server:latest
+docker push absh12/multi-workeri:lastest
 
-docker push abbas/multi-client:$SHA
-docker push abbas/multi-server:$SHA
-docker push abbas/multi-worker:$SHA
+docker push absh12/multi-client:$SHA
+docker push absh12/multi-server:$SHA
+docker push absh12/multi-worker:$SHA
 
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=abbas/multi-server:$SHA
-kubectl set image deployments/client-deployment client=abbas/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=abbas/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=absh12/multi-server:$SHA
+kubectl set image deployments/client-deployment client=absh12/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=absh12/multi-worker:$SHA
 
 
